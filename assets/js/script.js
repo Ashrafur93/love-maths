3fe87c81fc7c40cd3d823b716d722ac7e4ebbb56
Ashrafur Ahmed <ashrafur_ahmed@hotmail.co.uk>
2024-12-10 00:15:42 +0000

    Display additon question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,65 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
-            let buttons = document.getElementsByName("button");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-            for (let button of buttons) {
-                button.addEventListener("click", function () {
-                        if (this.getAttribute("data-type") === "submit") {
-                            alert("You clicked Submit!");
-                        } else {
-                            let gameType = this.getAttribute("data-type");
-                            alert(`You clicked ${gameType}`);
-                        }
-                    }
-                }
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
-            /**
-             * The main game "loop", called when the script is first loaded and after the user's answer has been processed
-             */
-            function runGame() {
-                let num1 = Math.floor(Math.random() * 25) + 1;
-                let num2 = Math.floor(Math.random() * 25) + 1;
-            }
+        });
+    }
+    runGame("addition");
+});
+/**
+ * The main game "loop", called when the script is first loaded and after the user's answer has been processed
+ */
+function runGame(gameType) {
 
-            function checkAnswer() {
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-            }
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
+}
 
-            function calculateCorrectAnswer() {
+function checkAnswer() {
 
-            }
+}
 
-            function incrementScore() {
+function calculateCorrectAnswer() {
 
-            }
+}
 
-            function incrementWrongAnswer() {
+function incrementScore() {
 
-            }
+}
 
-            function displayAdditionalQuestion() {
+function incrementWrongAnswer() {
 
-            }
+}
 
-            function displaySubtractQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+}
 
-            }
+function displaySubtractQuestion() {
 
-            function displayMultiplyQuestion() {
+}
 
-            }
+function displayMultiplyQuestion() {
+
+}
